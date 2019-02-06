788844468504d3b61d9c0867933889a2ed22e845
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-02-05 21:32:52 -0600

    created test for display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,58 @@
-// Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+
+
+describe('<Display/>', () => {
+
+    it('should display open and unlocked', () => {
+       const { getByText } = render(<Display closed={false} locked={false} />);
+       const open = getByText(/open/i);
+       const unlocked = getByText(/unlocked/i);
+       expect(open).toHaveTextContent(/open/i);
+       expect(open).toHaveClass('green-led');
+       expect(unlocked).toHaveTextContent(/unlocked/i);
+       expect(unlocked).toHaveClass('green-led');
+    });
+
+    it('should use green-led when open and unlocked', () => {
+       const { getByText } = render(<Display closed={false} locked={false} />);
+       const open = getByText(/open/i);
+       const unlocked = getByText(/unlocked/i);
+       expect(open).toHaveClass('green-led');
+       expect(unlocked).toHaveClass('green-led');
+    });
+ 
+     it('should display closed and unlocked', () => {
+       const { getByText } = render(<Display closed={true} locked={false} />);
+       const closed = getByText(/closed/i);
+       const unlocked = getByText(/unlocked/i);
+       expect(closed).toHaveTextContent(/closed/i);
+       expect(unlocked).toHaveTextContent(/unlocked/i);
+    });
+    
+    it('should use red-led class when closed and green-led when unlocked', () => {
+       const { getByText } = render(<Display closed={true} locked={false} />);
+       const closed = getByText(/closed/i);
+       const unlocked = getByText(/unlocked/i);
+       expect(closed).toHaveClass('red-led');
+       expect(unlocked).toHaveClass('green-led');
+    });
+
+    it('should display closed and locked', () => {
+       const { getByText } = render(<Display closed={true} locked={true} />);
+       const closed = getByText(/closed/i);
+       const locked = getByText('Locked');
+       expect(closed).toHaveTextContent(/closed/i);
+       expect(locked).toHaveTextContent(/locked/i);
+    });
+    
+    it('should use red-led when closed and locked', () => {
+       const { getByText } = render(<Display closed={true} locked={true} />);
+       const closed = getByText(/closed/i);
+       const locked = getByText('Locked');
+       expect(closed).toHaveClass('red-led');
+       expect(locked).toHaveClass('red-led');
+    });
+ });
